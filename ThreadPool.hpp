856ed7ae98af60c303a45466593bedab5867be77
refractiOOn <etozhesotnikow@gmail.com>
2023-03-05 12:44:59 +0200

    class Executable deleted, new implementation of Enqueue

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,21 +1,76 @@
-#include "Executable.hpp"
 #include <vector>
 #include <queue>
 #include <thread>
 #include <mutex>
+#include <future>
+#include <memory>
+#include <functional>
 #include <condition_variable>
 
 class ThreadPool
 {
 public:
-    ThreadPool(std::size_t numThreads);
-    ~ThreadPool();
+    typedef std::function<void()> Task;
 
-    void Enqueue(Executable&& task);
+    ThreadPool(std::size_t numThreads)
+    {
+        m_threads.resize(numThreads);
+        Start();
+    }
+    ~ThreadPool()
+    {
+        Stop();
+    }
+
+    template<typename Func, typename... Args>
+    auto Enqueue(Func&& func, Args&&... args) -> std::future<decltype(func(std::declval<Args>()...))>
+    {
+        auto wrapper = std::make_shared<std::packaged_task<decltype(func(std::declval<Args>()...)) ()>>(
+            std::bind(std::forward<Func>(func), std::forward<Args>(args)...));
+
+        {
+            std::unique_lock lock(m_taskMutex);
+            m_tasks.emplace([=] {
+                (*wrapper)();
+            });
+        }
+        m_taskVar.notify_one();
+
+        return wrapper->get_future();
+    }
 
 private:
-    void Start();
-    void Stop() noexcept;
+    void Start()
+    {
+        std::ranges::for_each(std::ranges::begin(m_threads), std::ranges::end(m_threads), [&](auto& el) {
+            el = std::jthread([&] {
+                while (true)
+                {
+                    Task task;
+
+                    {
+                        std::unique_lock lock(m_taskMutex);
+                        m_taskVar.wait(lock, [&] { return m_stopping || !m_tasks.empty(); } );
+                        if (m_stopping && m_tasks.empty()) break;
+
+                        task = std::move(m_tasks.front());
+                        m_tasks.pop();
+                    }
+
+                    task();
+                }
+            }); 
+        });
+    }
+    void Stop() noexcept
+    {
+        {
+            std::lock_guard lock(m_taskMutex);
+            m_stopping = true;
+        }
+
+        m_taskVar.notify_all();
+    }
 
 private:
     std::vector<std::jthread> m_threads;
@@ -24,6 +79,6 @@ private:
     bool m_stopping = false;
     std::mutex m_taskMutex;
 
-    std::queue<Executable> m_tasks;
+    std::queue<Task> m_tasks;
 
 };
--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
-#include <chrono>
 #include <atomic>
+#include <vector>
 #include "ThreadPool.hpp"
 
-void Increment(std::atomic<std::size_t>& num)
+int Plus(int a, int b, std::atomic<std::size_t>& totalSum)
 {
-    ++num;
+    int localSum = a + b;
+    totalSum += localSum;
+    return localSum;
 }
 
 int main(int, char**)
 {
     std::cout << "Hello, world!\n";
 
-    std::atomic<std::size_t> num = 0;
+    std::atomic<std::size_t> totalSum = 0;
+    std::vector<std::future<int>> futures;
 
     ThreadPool pool(std::jthread::hardware_concurrency());
-    for (std::size_t i = 0; i < 1000000; ++i)
+    for (int i = 0; i < 100000; ++i)
     {
-        pool.Enqueue(Executable(&Increment, std::ref(num)));
+        auto res = pool.Enqueue(Plus, 10, 20, std::ref(totalSum));
+        futures.push_back(std::move(res));
     }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-    std::cout << num << std::endl;
+    for (auto& el : futures) el.get(); // waiting for finish
+    
+    std::cout << "Total sum: " << totalSum << std::endl;
 }
